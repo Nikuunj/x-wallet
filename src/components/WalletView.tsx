6de2dd72e7d601f620e41ae622d042a50b43638e
Nikunj <mnikunj262@gmail.com>
2025-04-17 16:23:35 +0530

    implement: solana wallet is working with onCurve

--- src/components/WalletView.tsx
+++ src/components/WalletView.tsx
@@ -1,21 +1,38 @@
 import { memo } from "react"
-import { useRecoilState } from "recoil"
-import { mnemonicsState } from "../store/mnemonics"
+import { useRecoilState, useRecoilValue } from "recoil"
+import { account_idx, keyPairSelector, mnemonicsState } from "../store/mnemonics"
 import { generateMnemonic } from "bip39";
 
 
 function WalletView() {
     const [mnemonics, setMnemonics] = useRecoilState(mnemonicsState);
-
+    const keyPairArr = useRecoilValue(keyPairSelector);
+    const [account, setAccount] = useRecoilState(account_idx);
     function generate() {
         const genratedMemonics = generateMnemonic()
         localStorage.setItem('mnemonics', genratedMemonics)
         setMnemonics(genratedMemonics);
     }
     function clearAll() {
+        setAccount(0);
         setMnemonics('');
         localStorage.clear();
     }
+
+    function addMore() {
+        const number = account + 1;
+        localStorage.setItem('numberOfAcc', number.toString());
+        setAccount(pre => pre + 1);
+    }
+
+    const renderAcc =  keyPairArr.map((val, idx) => (
+        <div key={idx}>
+            {val.publicKey}
+            <br />
+            {val.privateKey}
+            <hr />
+        </div>
+    ))
     return (
         <div>
             {mnemonics}
@@ -24,6 +41,8 @@ function WalletView() {
                 click me to generate
             </div>}
             { mnemonics && <div onClick={clearAll}>Clear all</div>}
+            { mnemonics && <div onClick={addMore}> Add more</div>}
+            {renderAcc}
         </div>
     )
 }
--- src/store/mnemonics.ts
+++ src/store/mnemonics.ts
@@ -1,4 +1,9 @@
+import { mnemonicToSeedSync } from 'bip39';
+import { derivePath } from 'ed25519-hd-key';
+import bs58 from 'bs58'
+import { Keypair } from '@solana/web3.js'
 import { atom, selector } from 'recoil'
+import nacl from 'tweetnacl';
 
 interface keyPair {
     privateKey: string;
@@ -17,17 +22,52 @@ export const mnemonicsState = atom<string>({
     key: 'mnemonicsState',
     default: seletDefault
 })
-const keyPairSelector = selector({
-    key: 'keyPairSelector',
+
+const seedSelector = selector<string>({
+    key: 'seedSelector',
     get: ({ get }) => {
-        const keyPair = localStorage.getItem('keyPair');
-        if(!keyPair) return [];
-        const loopRun = parseInt(keyPair);
-        
-        
+        const mnemonics = get(mnemonicsState);
+        if(mnemonics) return mnemonicToSeedSync(mnemonics).toString('hex')
+        return ''
     }
 })
-export const keyPairState = atom<keyPair[]>({
-    key: 'keyPairState',
-    default: []
+export const keyPairSelector = selector<keyPair[]>({
+    key: 'keyPairSelector',
+    get: ({ get }) => {
+        const numberOfAcc = get(account_idx);
+        if(!numberOfAcc) return []
+        const seed = get(seedSelector);
+        
+        console.log(numberOfAcc);
+        
+        const keyPairArr: keyPair[] = Array(numberOfAcc).fill(0).map(( _ , idx) => {
+            const path = `m/44'/501'/${idx}'/0'`
+            const derivedSeed = derivePath(path, seed).key;
+            const privateKeyUint8 = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
+            const privateKey = bs58.encode(privateKeyUint8);
+            const publicKey = Keypair.fromSecretKey(privateKeyUint8).publicKey.toBase58();
+
+            return {
+                privateKey: privateKey,
+                publicKey: publicKey
+            }
+        })
+
+        console.log(keyPairArr);
+        
+        return keyPairArr
+    }
+})
+
+const selectAccountIdx = selector<number>({
+    key: 'selctorAccountIdx',
+    get: () => {
+        const numberOfAccount = localStorage.getItem('numberOfAcc');
+        if(numberOfAccount) return  parseInt(numberOfAccount)
+        return 0;
+    }
+})
+export const account_idx = atom<number>({
+    key: 'account_idx',
+    default: selectAccountIdx
 })
