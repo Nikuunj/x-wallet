d223a5cee0aecf361eb417737996a95cbd13acf6
Nikunj <mnikunj262@gmail.com>
2025-04-19 13:17:40 +0530

    remove: unwanted content

--- src/utils/copyText.ts
+++ src/utils/copyText.ts
@@ -1,6 +1,4 @@
-import { useState } from "react";
 
-export const [notify, setNotify] = useState<boolean>(false)
 
 export function copyHandle(text: string){
     navigator.clipboard.writeText(text);
